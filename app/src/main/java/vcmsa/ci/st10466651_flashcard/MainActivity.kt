955665da5a9f626735caf125d0bcbd113e5dcc2f
ST10466651 <ST10466651@vcconnect.edu.za>
2025-05-06 12:21:28 +0200

    Update

--- app/src/main/java/vcmsa/ci/st10466651_flashcard/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/st10466651_flashcard/MainActivity.kt
@@ -1,12 +1,31 @@
 package vcmsa.ci.st10466651_flashcard
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+
+    val questions = arrayOf(
+        ""
+    )
+
+    val answers = arrayOf(
+        ""
+    )
+
+    val useranswer = arrayOf(
+        "",
+        "",
+        "",
+        "",
+        ""
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +35,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val knowledge = findViewById<TextView>(R.id.textInfo)
+
+
+        knowledge.text = "Welcome to BrainBank - the ultimate flashcard app designed to supercharge your learning! Organize, review and retain information effortlessly. Study smarter, not harder and start building your brainpower today"
     }
 }
